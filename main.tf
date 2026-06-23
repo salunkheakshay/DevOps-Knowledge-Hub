c9883e742ed945cdaf3cb1a319007748d38b5e3f
Akshay Salunkhe <101976455+salunkheakshay@users.noreply.github.com>
2026-06-23 11:53:19 +0530

    Delete main.tf

--- main.tf
+++
@@ -1,8 +0,0 @@
-provider "aws" {
-    region = "us-west-2"  # Set your desired AWS region
-}
-
-resource "aws_instance" "test" {
-    ami           = "ami-0c55b159cbfafe1f0"  # Specify an appropriate AMI ID
-    instance_type = "t2.micro"
-}
